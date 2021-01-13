8fe56d777a6195535772873a49266a75869e947d
Robert Kowalski <rok@kowalski.gd>
2021-01-13 12:45:17 +0100

    trades & candles coverage

--- test/backtest-offline.js
+++ test/backtest-offline.js
@@ -6,11 +6,12 @@ const assert = require('assert')
 
 const { execOffline } = require('../')
 
-const candles = require('./fixtures/btc-usd-candles.js')
 const getStrategy = require('./get-strategy')
 
 describe('basic', () => {
   it('just candles: creates trades, calcs prices, pl and fees', async () => {
+    const candles = require('./fixtures/btc-usd-candles.js')
+
     const args = { candles, trades: [] }
     const strategy = getStrategy({ tf: '15m', symbol: 'tBTCUSD' })
     const state = await execOffline(strategy, args)
@@ -39,6 +40,33 @@ describe('basic', () => {
         39163, 40650,
         39972, 41334
       ]
+    )
+  })
+
+  it('trades & candles', async () => {
+    const candlesSource = require('./fixtures/eth-eur-candles.js')
+    const tradesSource = require('./fixtures/eth-eur-trades.js')
+
+    const args = { candles: candlesSource, trades: tradesSource }
+    const strategy = getStrategy({ tf: '5m', symbol: 'tETHEUR' })
+    const state = await execOffline(strategy, args)
+
+    const { trades } = state.strategy
+    assert.strictEqual(trades.length, 6)
+
+    const { fees, pl, prices } = trades.reduce((acc, el) => {
+      acc.pl += el.pl
+      acc.fees += el.fee
+      acc.prices.push(el.price)
+
+      return acc
+    }, { fees: 0, pl: 0, prices: [] })
+
+    assert.strictEqual(fees, 12.77324954092)
+    assert.strictEqual(pl, -39.73275680092024)
+    assert.deepStrictEqual(
+      prices,
+      [1075.09213886, 1056.9326316, 1049.3, 1067.4, 1064.3, 1073.6]
     )
   })
 })
